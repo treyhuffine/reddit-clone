48fff9e492bea94618605370edcbd11c9c283ee8
Trey Huffine <treyhuffine@gmail.com>
2015-06-25 14:28:32 -0700

    Implement for link service

--- script.js
+++ script.js
@@ -8,21 +8,29 @@ angular.module('reddit', ["ngRoute"])
       templateUrl: "list.html"
     })
     .when("/new", {
-      controll: "NewLinkCtrl",
+      controller: "NewLinkCtrl",
       templateUrl: "new.html"
     })
     .otherwise({
       redirectTo: "/"
     });
 })
-.service('linkService', function(){
+.service('linkService', function() {
   this.links = [
     { title: 'Google', url: "http://google.com", description: "Google magic" },
     { title: 'Yahoo', url: "http://yahoo.com", description: "Yahooooo" }
   ];
+  this.storeLink = function(newLink) {
+    this.links.unshift(newLink);
+  };
  })
-.controller('NewLinkCtrl', function($scope) {
-
+.controller('NewLinkCtrl', function($scope, $location, linkService) {
+  $scope.newLink = {};
+  $scope.addLinkAndRedirect = function() {
+    linkService.storeLink($scope.newLink);
+    $scope.newLink = {};
+    $location.path("/");
+  };
  })
 .controller('ListCtrl', function($scope, linkService){
   $scope.links = linkService.links;
