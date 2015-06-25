ecfdb0cffa9e6656bd75e2ccf357d8eda50a57c3
Trey Huffine <treyhuffine@gmail.com>
2015-06-25 12:40:39 -0700

    Implement angular

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 'use strict';
 
-angular.module('reddit', [])
-.controller('MainCtrl', function($scope){
+angular.module('reddit', ["ngRoute"])
+.controller('MainCtrl', function($scope, $http){
+  $scope.links = [
+    { title: 'Google', url: "http://google.com", description: "Google magic" },
+    { title: 'Yahoo', url: "http://yahoo.com", description: "Yahooooo" }
+  ];
+  $scope.fillForm = function() {
+        $scope.links.unshift($scope.link);
+        $scope.link = {};
+  };
 });
